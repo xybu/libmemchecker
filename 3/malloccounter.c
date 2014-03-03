235168311f04df27a932f0298fa6bb4a869ef345
Xiangyu Bu <xybu92@live.com>
2014-03-02 22:08:58 -0500

    Working on method 3

--- 3/malloccounter.c
+++ 3/malloccounter.c
@@ -1,22 +1,74 @@
 #define _GNU_SOURCE
 
 #include <dlfcn.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #define SECRET_FD 17
 
-static long counter = 0;
+static void *(*real_calloc)(size_t, size_t) = NULL;
+static void *(*real_malloc)(size_t) = NULL;
+static void *(*real_realloc)(size_t) = NULL;
+static void (*real_free)(void *) = NULL;
 
-#define GET_REAL(f) do { \
-    if (real_ ## f == NULL) { \
-        real_ ## f = dlsym(RTLD_NEXT, #f); \
-    } \
-} while (0)
 
-void printCount()
-{
+/**
+ * Maintain a linked list structure for memory allocated
+ */
+typedef struct lnode {
+	unsigned long int addr;
+	size_t size;
+	struct lnode* next;
+} Lnode;
+
+static Lnode* head = NULL;
+static size_t bytesUnfreed = 0;
+
+static void __lib_addNode(unsigned long a, size_t t){
+	Lnode *n = (Lnode *) malloc(sizeof(Lnode));
+	n->addr = a;
+	n->size = t;
+	n->next = NULL;
+	
+	if (!head){
+		head = n;
+	} else {
+		head->next = n;
+	}
+}
+
+static void __lib_updateNode(unsigned long oldAddr, unsigned long newAddr, size_t t){
+	// locate the node
+	Lnode *temp = head;
+	while (temp != NULL){
+		if (temp->addr == oldAddr) break;
+		temp = temp->next;
+	}
+	
+	if (temp == NULL)
+		perror("Node does not exist.");
+	else {
+		temp->addr = newAddr;
+		temp->size = t;
+	}
+}
+
+static void __lib_delNode(unsigned long a){
+	Lnode *temp = head, prev = NULL;
+	while (temp != NULL){
+		if (temp->addr == oldAddr){
+			if (prev == NULL) head = temp->next;
+			else prev->next = temp->next;
+			free(temp);
+			break;
+		}
+		prev = temp;
+		temp = temp->next;
+	}
+}
+
+void printCount(){
     char buf[1024];
     int sz;
     sz = sprintf(buf, "%ld\n", counter);
@@ -25,34 +77,38 @@ void printCount()
 }
 
 __attribute__((constructor))
-void mallocCounter()
-{
+void mallocCounter(){
+	do {
+		real_malloc = dlsym(RTLD_NEXT, "malloc");
+		real_calloc = dlsym(RTLD_NEXT, "calloc");
+		real_realloc = dlsym(RTLD_NEXT, "realloc");
+		real_free = dlsym(RTLD_NEXT, "free");
+	} while (real_malloc && real_calloc && real_realloc && real_free);
+	
     dup2(1, SECRET_FD);
     close(1);
     close(2);
+	
     atexit(printCount);
 }
 
-void *calloc(size_t nmemb, size_t size)
-{
-    static void *(*real_calloc)(size_t, size_t) = NULL;
-    counter++;
-    GET_REAL(calloc);
+void *calloc(size_t nmemb, size_t size){
+    
+    
     return real_calloc(nmemb, size);
 }
 
-void *malloc(size_t size)
-{
-    static void *(*real_malloc)(size_t) = NULL;
-    counter++;
-    GET_REAL(malloc);
+void *malloc(size_t size){
+    
     return real_malloc(size);
 }
 
-void free(void *ptr)
-{
-    static void (*real_free)(void *) = NULL;
-    counter--;
-    GET_REAL(free);
+void *realloc(void *ptr, size_t size){
+    
+    return real_realloc(ptr, size);
+}
+
+void free(void *ptr){
+    
     real_free(ptr);
 }
