f9cf3482de9b07cfc4c44c2611a09319c62e9883
Xiangyu Bu <xybu92@live.com>
2014-03-22 20:47:05 -0400

    The oldest malloccounter.c

--- 3/malloccounter.c
+++ 3/malloccounter.c
@@ -1,74 +1,58 @@
 #define _GNU_SOURCE
 
 #include <dlfcn.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-static int fd_out = 1;
-static void *(*real_calloc)(size_t, size_t) = NULL;
-static void *(*real_malloc)(size_t) = NULL;
-static void *(*real_realloc)(size_t) = NULL;
-static void (*real_free)(void *) = NULL;
+#define SECRET_FD 17
 
-void flushLog(){
-	close(fd_out);
-	sigset(SIGSTOP, SIG_DFL);
-	sigset(SIGTSTP, SIG_DFL);
-	_exit(0);
+static long counter = 0;
+
+#define GET_REAL(f) do { \
+    if (real_ ## f == NULL) { \
+        real_ ## f = dlsym(RTLD_NEXT, #f); \
+    } \
+} while (0)
+
+void printCount()
+{
+    char buf[1024];
+    int sz;
+    sz = sprintf(buf, "%ld\n", counter);
+    sz = write(SECRET_FD, buf, sz);
+    (void) sz;
 }
 
-void flushLogWrapper(int x){
-	flushLog();
+__attribute__((constructor))
+void mallocCounter()
+{
+    dup2(1, SECRET_FD);
+    close(1);
+    close(2);
+    atexit(printCount);
 }
 
-int recordLog(char* format, ...) {
-	char buf[1024] = {0};
-	int bytesWritten = 0;
-	
-	if (format){
-		va_list valist;
-		va_start(valist, format);
-		bytesWritten = vsnprintf(buf, 1024, format, valist);
-		va_end(valist);
-	}
-	
-	write(fd_out, buf, bytesWritten);
+void *calloc(size_t nmemb, size_t size)
+{
+    static void *(*real_calloc)(size_t, size_t) = NULL;
+    counter++;
+    GET_REAL(calloc);
+    return real_calloc(nmemb, size);
 }
 
-__attribute__((constructor)) void mallocCounter() {
-	do {
-		real_malloc = dlsym(RTLD_NEXT, "malloc");
-		real_calloc = dlsym(RTLD_NEXT, "calloc");
-		real_realloc = dlsym(RTLD_NEXT, "realloc");
-		real_free = dlsym(RTLD_NEXT, "free");
-	} while (!real_malloc || !real_calloc || !real_realloc || !real_free);
-	
-	fd_out = creat("libmemchecker_log.txt", 0664);
-    
-	sigset(SIGTSTP, flushLogWrapper);
-    atexit(flushLog);
+void *malloc(size_t size)
+{
+    static void *(*real_malloc)(size_t) = NULL;
+    counter++;
+    GET_REAL(malloc);
+    return real_malloc(size);
 }
 
-void *calloc(size_t nmemb, size_t size){
-	void *lptr = real_calloc(nmemb, size);
-	recordLog("+\t%lu\t%p\tcalloc\n", (long unsigned) size * nmemb, lptr);
-	return lptr;
-}
-
-void *malloc(size_t size){
-	void *lptr = real_malloc(size);
-	recordLog("+\t%lu\t%p\tmalloc\n", (long unsigned) size, lptr);
-	return lptr;
-}
-
-void *realloc(void *ptr, size_t size){
-    void *lptr = real_realloc(ptr, size);
-	fprintf(fd_output, "~\t%lu\t%p\t%p\trealloc\n", (long unsigned) size, ptr, lptr);
-    return lptr;
-}
-
-void free(void *ptr){
-    fprintf(fd_output, "-\t%p\n", ptr);
+void free(void *ptr)
+{
+    static void (*real_free)(void *) = NULL;
+    counter--;
+    GET_REAL(free);
     real_free(ptr);
 }
